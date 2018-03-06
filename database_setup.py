26c2e4e328236c2e478cecc973fca45e871a2250
ChadTC <codecrab.me@gmail.com>
2018-03-05 20:30:24 -0800

    model (database) classes built for Restaurant and MenuItem

--- database_setup.py
+++ database_setup.py
@@ -10,6 +10,23 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 
+class Restaurant(Base):
+__tablename__ = 'restaurant'
+name = Column(String(65), nullable = False)
+id = Column(Integer, primary_key = True)
+
+class MenuItem(Base);
+__tablename__ = 'menu_item'
+name = Column(String(65), nullable = False)
+id = Column(Integer, primary_key = True)
+restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
+restaurant = relationship(Restaurant)
+course = Column(String(200))
+description = Column(String(200))
+price = Column(String(10))
+
+
+
 ######insert at end of file #######
 engine = create_engine('sqlite:///restaurantmenu.db')
 
